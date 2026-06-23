6ab70a4af73381075972e4637d96ceec39295677
rindicomfort <kwarpojonathanrindi@gmail.com>
2026-06-23 20:39:35 +0100

    feat: implement QUIC stream multiplexer packet reordering and flow control

---
+++ src/net/quic/mod.rs
@@ -0,0 +1,11 @@
+#[path = "reorder-buffer.rs"]
+pub mod reorder_buffer;
+
+#[path = "stream-multiplexer.rs"]
+pub mod stream_multiplexer;
+
+#[path = "flow-controller.rs"]
+pub mod flow_controller;
+
+#[path = "retransmission-requestor.rs"]
+pub mod retransmission_requestor;
